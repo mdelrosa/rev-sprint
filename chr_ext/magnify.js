73671cc710c810b0962bd82ef80c945284e263f4
Mason <mason@mason-VirtualBox.(none)>
2013-04-07 23:19:36 -0400

    extension that will make facebook frustrating (lol)

--- chr_ext/magnify.js
+++ chr_ext/magnify.js
@@ -1,12 +1,26 @@
-$(document).ready(function() {
+console.log('magnify.js loaded');
 
-	console.log('magnify.js loaded');
-
-	$('img').mouseover(function() {
-		$('this').fadeTo('slow', 0.5);
-	});
-
-	$('img').mouseout(function() {
-		$('this').fadeTo('slow', 1);
-	});	
+chrome.tabs.onUpdated.addListener(function() {
+  chrome.tabs.getSelected(null,function(tab) {
+    var tablink = tab.url;
+    var badlink = 'facebook'
+    console.log(tablink.indexOf(badlink))
+    if (tablink.indexOf(badlink) > -1) {
+      console.log('badlink')
+      chrome.tabs.create({url: "http://knowyourmeme.com/memes/o-rly"}, function(popup) {
+  		console.log(popup);
+  		popup.show;
+      });
+    }
+  })
 });
+
+  $('img').mouseenter(function() {
+    console.log('mousein');
+    $(this).fadeTo('slow', 0.5);
+  });
+
+  $('img').mouseout(function() {
+    console.log('mouseout');
+    $(this).fadeTo('slow', 1);
+  });  
